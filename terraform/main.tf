53922f5b524c694e3be29ea38c78e0c444373cac
jettbtirrell <jett.b.tirrell@gmail.com>
2026-07-25 22:08:16 -0400

    Added s3:GetReplicationConfiguration

--- terraform/main.tf
+++ terraform/main.tf
@@ -443,7 +443,8 @@ resource "aws_iam_role_policy" "backend_deploy_permissions" {
           "s3:GetAccelerateConfiguration",
           "s3:GetBucketRequestPayment",
           "s3:GetBucketLogging",
-          "s3:GetLifecycleConfiguration"
+          "s3:GetLifecycleConfiguration",
+          "s3:GetReplicationConfiguration"
         ]
         Resource = "arn:aws:s3:::jett-cloud-resume-2026"
       },
