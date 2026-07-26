a58ba51f6a2bad492dbd71beb659d3367cafcc33
jettbtirrell <jett.b.tirrell@gmail.com>
2026-07-25 21:03:03 -0400

    Added s3:GetBucketWebsite to S3ReadForState of backend_deploy_permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -438,7 +438,8 @@ resource "aws_iam_role_policy" "backend_deploy_permissions" {
           "s3:GetEncryptionConfiguration",
           "s3:ListBucket",
           "s3:GetBucketLocation",
-          "s3:GetBucketCORS"
+          "s3:GetBucketCORS",
+          "s3:GetBucketWebsite"
         ]
         Resource = "arn:aws:s3:::jett-cloud-resume-2026"
       },
